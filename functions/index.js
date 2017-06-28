184fa962337446c8273e5ccbe78a7280187a079a
Carson Bruce <carson_bruce@bnz.co.nz>
2017-06-28 15:54:34 +1200

    Added blur image and thumbnail

--- functions/index.js
+++ functions/index.js
@@ -28,12 +28,16 @@ const LOCAL_TMP_FOLDER = '/tmp/';
 const base64Img = require('base64-img');
 
 // Max height and width of the thumbnail in pixels.
-const THUMB_MAX_HEIGHT = 40;
-const THUMB_MAX_WIDTH = 40;
-const THUMB_QUALITY = 25;
-const THUMB_BLUR = '0x8';
+const THUMB_MAX_HEIGHT = 200;
+const THUMB_MAX_WIDTH = 200;
+const THUMB_QUALITY = 50;
+const BLUR_MAX_HEIGHT = 40;
+const BLUR_MAX_WIDTH = 40;
+const BLUR_QUALITY = 25;
+
 // Thumbnail prefix added to file names.
-const THUMB_PREFIX = 'blur_';
+const THUMB_PREFIX = 'thumb_';
+const BLUR_PREFIX = 'blur_';
 admin.initializeApp(functions.config().firebase);
 const ref = admin.database().ref();
 
@@ -53,9 +57,11 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
   const fileDir = filePathSplit.join('/') + (filePathSplit.length > 0 ? '/' : '');
   const fileId = fileName.split('.')[0];
   const thumbFilePath = `${fileDir}${THUMB_PREFIX}${fileName}`;
+  const blurFilePath = `${fileDir}${BLUR_PREFIX}${fileName}`;
   const tempLocalDir = `${LOCAL_TMP_FOLDER}${fileDir}`;
   const tempLocalFile = `${tempLocalDir}${fileName}`;
   const tempLocalThumbFile = `${LOCAL_TMP_FOLDER}${thumbFilePath}`;
+  const tempLocalBlurFile = `${LOCAL_TMP_FOLDER}${blurFilePath}`;
   const thumbFile = bucket.file(thumbFilePath);
 
 // Exit if this is triggered on a file that is not an image.
@@ -65,7 +71,7 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
   }
 
 // Exit if the image is already a thumbnail.
-  if (fileName.startsWith(THUMB_PREFIX)) {
+  if (fileName.startsWith(THUMB_PREFIX) || fileName.startsWith(BLUR_PREFIX)) {
     console.log('Already a Thumbnail.');
     return;
   }
@@ -84,22 +90,34 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
     });
   }).then(() => {
     console.log('The file has been downloaded to', tempLocalFile);
-// Generate a thumbnail using ImageMagick.
-    return spawn('convert', [
-      tempLocalFile,
-      '-thumbnail', `${THUMB_MAX_WIDTH}x${THUMB_MAX_HEIGHT}>`,
-      '-quality', `${THUMB_QUALITY}`,
-      // '-blur', `${THUMB_BLUR}`,
-      tempLocalThumbFile
+    // Generate a thumbnail using ImageMagick.
+    return Promise.all([
+      spawn('convert', [
+        tempLocalFile,
+        '-thumbnail', `${THUMB_MAX_WIDTH}x${THUMB_MAX_HEIGHT}>`,
+        '-quality', `${THUMB_QUALITY}`,
+        tempLocalThumbFile
+      ]),
+      spawn('convert', [
+        tempLocalFile,
+        '-thumbnail', `${BLUR_MAX_WIDTH}x${BLUR_MAX_HEIGHT}>`,
+        '-quality', `${BLUR_QUALITY}`,
+        tempLocalBlurFile
+      ])
     ]);
   }).then(() => {
     console.log('Thumbnail created at', tempLocalThumbFile);
 // Uploading the Thumbnail.
-    return bucket.upload(tempLocalThumbFile, {
-      destination: thumbFilePath
-    })
+    return Promise.all([
+      bucket.upload(tempLocalThumbFile, {
+        destination: thumbFilePath
+      }),
+      bucket.upload(tempLocalBlurFile, {
+        destination: blurFilePath
+      })
+    ])
   }).then(() => {
-    console.log('Thumbnail uploaded to Storage at', thumbFilePath);
+    console.log('Thumbnail uploaded to Storage at', thumbFilePath, blurFilePath);
   }).then(() => {
     const config = {
       action: 'read',
@@ -108,17 +126,22 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
 // Get the Signed URL for the thumbnail and original images
     return Promise.all([
       thumbFile.getSignedUrl(config),
+      blurFile.getSignedUrl(config),
       file.getSignedUrl(config),
     ]);
   }).then(results => {
     console.log('Got Signed URL');
     const thumbResult = results[0];
-    const originalResult = results[1];
+    const blurResult = results[1];
+    const originalResult = results[2];
+
     const thumbFileUrl = thumbResult[0];
+    const blurFileUrl = blurResult[0];
     const fileUrl = originalResult[0];
 
+    // Get the blur file as a base64 string
     return new Promise((success, failure) => {
-      base64Img.base64(tempLocalThumbFile, (err, data) =>
+      base64Img.base64(tempLocalBlurFile, (err, data) =>
         err
           ? failure(err)
           : success(data)
@@ -129,10 +152,10 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
         .set({
           path: fileUrl,
           base64: base64,
-          thumbnail: thumbFileUrl
+          thumbnail: thumbFileUrl,
+          blur: blurFileUrl
         });
-    })
-
+    });
   }).catch(reason => {
     console.error(reason);
   });
