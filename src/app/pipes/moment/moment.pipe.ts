88ac82d8d95765dd0d3b056d3b2108416fb0c43f
Carson Bruce <carson.bruce@solnet.co.nz>
2017-07-04 21:09:39 +1200

    moment date format

--- src/app/pipes/moment/moment.pipe.ts
+++ src/app/pipes/moment/moment.pipe.ts
@@ -4,6 +4,6 @@ import * as moment from 'moment';
 @Pipe({name: 'moment'})
 export class MomentPipe implements PipeTransform {
   transform(value: string | number, format: string): string {
-    return moment(value).format('DD/MM/YYYY');
+    return moment(value).format('MMMM Do YYYY, h:mma');
   }
 }
