524914ff328c9bf5311f30c6aa08eb61a6196e6d
Carson Bruce <carson.bruce@solnet.co.nz>
2017-07-04 17:50:12 +1200

    stats progress

--- functions/function.stats.js
+++ functions/function.stats.js
@@ -8,11 +8,24 @@ module.exports = functions.database.ref('/events/{eventId}')
     // Get all events
     return admin.database().ref('events').once('value', (data) => {
 
-      let ids = Object.keys(data.val());
+      let val = data.val();
+      let ids = Object.keys(val);
+
+      let possum = count('possum', val);
+      let rat = count('rat', val);
+      let mouse = count('mouse', val);
+      let stoat = count('stoat', val);
 
       return admin.database().ref('stats').set({
         updatedAt: admin.database.ServerValue.TIMESTAMP,
         total: ids.length,
+        pests: {possum, rat, mouse, stoat}
       });
     });
   });
+
+
+function count(pest, val) {
+  return Object.keys(val)
+    .filter(key => val[key].pest === pest)
+}
